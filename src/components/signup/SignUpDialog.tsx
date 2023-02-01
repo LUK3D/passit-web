053ae5b602029da65dfbec4b0c049ca9aadb3fa0
Filipe Lukebana <filipelukebana@gmail.com>
2023-01-31 21:12:55 -0300

    Responsive Signup Dialog

--- src/components/signup/SignUpDialog.tsx
+++ src/components/signup/SignUpDialog.tsx
@@ -6,12 +6,12 @@ const SignUpDialog = () => {
 
     const {isOpen,toggle } = useSignUpDialogStore((state) => state);
     return ( 
-        <div className={` ${isOpen?'flex':'hidden'} fixed right-0 top-0 z-20 w-screen h-screen bg-black/50  flex-col justify-center items-center`}>
-            <div className="w-[600px] h-[700px] bg-white rounded-lg shadow-xl p-10 flex flex-col">
-                <h1 className="text-4xl font-bold">Falta pouco para fazeres parte do time PassiT</h1>
-                <p className="mt-10">Para começar, precisamos de algumas informações para validar a sua candidatura 😊</p>
+        <div className={` ${isOpen?'flex':'hidden'} p-2 md:p-10 lg:p-0 fixed right-0 top-0 z-20 w-screen h-screen bg-black/50  flex-col justify-center items-center`}>
+            <div className="w-full h-full md:w-[600px] md:h-[700px] bg-white rounded-lg shadow-xl p-5 md:p-10 flex flex-col overflow-y-auto">
+                <h1 className="text-xl md:text-4xl font-bold">Falta pouco para fazeres parte do time PassiT</h1>
+                <p className="mt-4 md:mt-10 text-xs md:text-sm">Para começar, precisamos de algumas informações para validar a sua candidatura 😊</p>
 
-                <div className="w-full p-2 border-2 rounded-lg mt-10">
+                <div className="w-full p-2 border-2 rounded-lg mt-5 md:mt-10">
                     <input className="w-full outline-none" placeholder="Nome Completo" type="text" />
                 </div>
                 <div className="w-full p-2 border-2 rounded-lg mt-5">
@@ -24,8 +24,8 @@ const SignUpDialog = () => {
                     <input className="w-full outline-none" placeholder="Email" type="text" />
                 </div>
 
-                <p className="mt-10 text-sm">É importante fornecer informações válidas pois serão usadas para entrar em contacto com você posteriormente</p>
-                <div className="w-full flex justify-between">
+                <p className="mt-4 md:mt-10 text-xs md:text-sm">É importante fornecer informações válidas pois serão usadas para entrar em contacto com você posteriormente</p>
+                <div className="w-full flex flex-col-reverse justify-between md:flex-row">
                         <button onClick={()=>toggle()} className='px-10 py-2.5 bg-white text-green-500 shadow border  mt-5 rounded-md'>
                             Cancelar
                         </button>
